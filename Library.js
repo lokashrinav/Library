83867ab49f5d11400230dfe96e14956d85725e07
Shrinav Loka <lokashrinav@gmail.com>
2022-09-04 17:05:30 -0400

    Used JS to create Cards on Click of Button

---
+++ Library.js
@@ -0,0 +1,23 @@
+let flip = document.querySelector(".flip");
+let newDiv = document.createElement("div");
+let currentDiv = document.getElementById("div1");
+
+
+document.getElementsByClassName('center')[0]
+        .addEventListener('click', function (event) {
+            let newDiv = document.createElement("div");
+            flip.appendChild(newDiv);
+            newDiv.classList.add("card");
+        });
+
+function Book() {
+    for(i in myLibrary){
+        this.Book
+    }
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
