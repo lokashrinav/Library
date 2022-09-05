e18ca647da5f9519070ab99a3ae588583a25433d
Shrinav Loka <lokashrinav@gmail.com>
2022-09-04 20:44:56 -0400

    Added popup to enter book informatin

--- Library.js
+++ Library.js
@@ -1,13 +1,26 @@
 let flip = document.querySelector(".flip");
-let newDiv = document.createElement("div");
 let currentDiv = document.getElementById("div1");
-
+let i = 0;
+let myLibrary = [];
+let pop = document.querySelector('.popup');
 
 document.getElementsByClassName('center')[0]
         .addEventListener('click', function (event) {
+          pop.classList.remove("popup");
+          pop.classList.add("popup-open");
+        });
+
+document.getElementsByClassName('enter')[0]
+        .addEventListener('click', function (event) {
+            pop.classList.remove("popup-open");
+            pop.classList.add("popup");
             let newDiv = document.createElement("div");
+            myLibrary[i] = newDiv;
             flip.appendChild(newDiv);
+            newDiv.value = i;
             newDiv.classList.add("card");
+            i++;
+            
         });
 
 function Book() {
